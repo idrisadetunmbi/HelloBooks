fe2743910fa72af9c949949c35498140a7bbd122
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-06 10:13:24 +0100

    Add RESTful "api" route

--- server/app.js
+++ server/app.js
@@ -10,8 +10,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.'
-}));
+app.use('/api', require('./routes/api'));
 
 module.exports = app;
