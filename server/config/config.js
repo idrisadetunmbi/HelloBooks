18e4b6c52421fa48fc724cc007ce46f9f32f0072
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-08 22:51:30 +0100

    Add database configs for other environments

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,20 @@ export default {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'hellobooks-dev',
+    database: process.env.DB_NAME,
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres'
   },
+  production: {
+    username: 'rzjphxev',
+    password: 'q92INLKzxtqf2Olln-joaLyjWt9LnmVq',
+    database: 'rzjphxev',
+    host: 'babar.elephantsql.com',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  test: {
+  }
 };
 
