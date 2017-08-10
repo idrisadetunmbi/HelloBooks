3f76be5bbe787e7db6b2cb5d875afa5b68b37714
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-10 16:16:11 +0100

    Reintegrate and define more tests

--- server/bin/bin.js
+++ server/bin/bin.js
@@ -1,9 +1,11 @@
 import http from 'http';
 import app from '../app';
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
 console.log(`Listening on port: ${port}`);
+
+export default server;
---
+++ server/test/user_routes_test.js
@@ -0,0 +1,102 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import userSeeders from '../seeders/user';
+import server from '../bin/bin';
+
+import models from '../models';
+
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+beforeEach((done) => {
+  models.sequelize.sync({ force: true }).then(() => {
+    done(null);
+  }).catch((errors) => {
+    done(errors);
+  });
+});
+
+describe('User sign up and sign in routes', () => {
+  describe('GET /', () => {
+    it('should return 404', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          assert.equal(res.statusCode, 404);
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/users/signup', () => {
+    it('should return status code 400 when no data is supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .end((err, res) => {
+          assert.equal(res.statusCode, 400);
+          assert.equal(res.body.message, 'No password supplied');
+          done();
+        });
+    });
+
+    it('should return 201 for a successful sign in when data is supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .type('form')
+        .send(userSeeders.signUp.fullUserDetails)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 201);
+          assert.equal(res.body.message, 'user successfully created');
+          done();
+        });
+    });
+
+    it('should return return 400 when username is not supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .type('form')
+        .send(userSeeders.signUp.nullUsername)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 400);
+          assert.equal(res.body.message, 'Invalid credentials supplied');
+          done();
+        });
+    });
+
+    it('should return return 400 when invalid email is supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .type('form')
+        .send(userSeeders.signUp.nullEmail)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 400);
+          assert.equal(res.body.message, 'Invalid credentials supplied');
+          done();
+        });
+    });
+
+    it('should return return 400 when invalid or no password is supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .type('form')
+        .send(userSeeders.signUp.nullPassword)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 400);
+          assert.equal(res.body.message, 'No password supplied');
+          done();
+        });
+    });
+
+    it('should return return 400 when invalid or no password is supplied', (done) => {
+      chai.request(server)
+        .post('/api/users/signup')
+        .type('form')
+        .send(userSeeders.signUp.nullPassword)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 400);
+          assert.equal(res.body.message, 'No password supplied');
+          done();
+        });
+    });
+  });
+});
