d839c5ad8e9b370f097414f0388af52e6b8f5d46
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-11 16:03:24 +0100

    Modify user database table schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    identifier: {
+    id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV1,
       primaryKey: true
@@ -14,28 +14,26 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-
     admin: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
     },
-
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
       validate: {
         isEmail: true
       }
     },
     membershipLevel: {
-      field: 'membership_level',
       type: DataTypes.ENUM,
-      values: ['regular', 'silver', 'gold', 'platinum']
+      values: ['regular', 'silver', 'gold', 'platinum'],
+      allowNull: false
     },
   }, {
     classMethods: {
       associate: (models) => {
+
       }
     }
   });
