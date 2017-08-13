9d3f1d3e4077d64a01760050790feea09b361f86
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-13 11:50:50 +0100

    Use express validator to first check sigin request body

--- server/controllers/requestValidators.js
+++ server/controllers/requestValidators.js
@@ -19,5 +19,24 @@ export default {
         next();
       }
     });
-  }
+  },
+
+  signInRequest: (req, res, next) => {
+    req.checkBody('username', 'username can only be 6 to 15 alphanumeric characters').notEmpty().len(6, 15).isAlphaNum();
+    req.checkBody('password', 'password can\'t be less than 6 characters').notEmpty().len(6, 20);
+    
+    req.getValidationResult().then((errors) => {
+      if (!errors.isEmpty()) {
+        let errorArray = errors.useFirstErrorOnly().array();
+
+        res.status(400).send({
+          message: 'invalid parameters supplied',
+          description: errorArray
+        });
+      } else {
+        // res.send('valid input');
+        next();
+      }
+    });
+  },
 };
