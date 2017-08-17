9eda2f8c330b5bf1b74a5397aabc9b9a2b7343fb
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-17 17:05:50 +0100

    Modify responses and add more edge cases

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,15 +4,13 @@ import dotenv from 'dotenv';
 
 import models from '../models/';
 
-import app from '../app';
-
 const User = models.User;
 const Book = models.Book;
 const BorrowHistory = models.BorrowHistory;
 
 dotenv.config({
   path: `${__dirname}/../../.env`
-})
+});
 
 export default {
   // POST - /users/signup
@@ -22,8 +20,7 @@ export default {
         username: req.body.username,
         password: hashedPassword,
         email: req.body.email,
-        membershipLevel: req.body.membershipLevel,
-        admin: req.body.admin
+        membershipLevel: req.body.membershipLevel
       })
         .then((user) => {
           user.password = undefined;
@@ -65,12 +62,13 @@ export default {
           }
         } else {
           res.status(401).send({
-            message: 'please verify the username or that you are registered'
+            message: 'username does not exist'
           });
         }
       })
       .catch(error => res.status(401).send({
-        message: 'please verify your details are correct'
+        message: 'please verify your details are correct',
+        error
       })); // User.findOne catch
   },
 
@@ -78,99 +76,128 @@ export default {
   // borrowed but has not returned 
   // GET​ - /api/users/:userId/books?returned=false
   getBorrowedBooks(req, res) {
-    BorrowHistory.findAll({
-      where: {
-        userId: req.params.userId,
-        returnStatus: req.query.returned
-      }
-    }).then(results => res.status(200).send(results))
-      .catch(error => res.status(500).send(error.message));
+    const returned = req.query.returned;
+    if (returned) {
+      BorrowHistory.findAll({
+        where: {
+          userId: req.params.userId,
+          returnStatus: req.query.returned
+        }
+      }).then(results => res.status(200).send(results))
+        .catch(error => res.status(500).send(error.message));
+    } else {
+      BorrowHistory.findAll({
+        where: {
+          userId: req.params.userId,
+        }
+      }).then(results => res.status(200).send(results))
+        .catch(error => res.status(500).send(error.message));
+    }
   },
 
   // An API route that allow a user to borrow a book
-  // POST​ : /api/users/:userId:/books
-  borrowBook(req, res) {
-    const userId = req.user.identifier;
+  // POST​ : /api/users/:userId/books
+  borrowBook(req, res, next) {
+    const userId = req.user;
     const bookId = req.body.bookId;
-
     // if user making the request is not equal to the signed in user
-    if (req.params.userId !== userId) {
-      res.status(400).send({ message: 'bad request: unequal users' });
-    } else {
-      return User.findById(userId).then((result) => {
-        if (!result) {
-          res.status(400).send({
-            result,
-            message: 'unknown user id'
-          });
-        } else if (result.admin) {
-          res.status(403).send({
-            message: 'admin user cannot borrow a book'
-          });
-        } else {
-          Book.findById(bookId).then((bookResult) => {
-            if (!bookResult) {
+    Book.findById(bookId).then((bookInstance) => {
+      if (bookInstance.quantity === 0) {
+        res.status(400).send({
+          message: 'there is not enough quantity of this book in the library'
+        });
+      } else {
+        BorrowHistory.findOne({
+          where: {
+            userId,
+            bookId
+            // returnStatus: false
+          }
+        }).then((borrowHistoryInstance) => {
+          if (borrowHistoryInstance) {
+            if (!borrowHistoryInstance.returnStatus) { // if user had previously borrowed this book without returning it
               res.status(400).send({
-                bookResult,
-                message: 'unknown book id'
-              });
-            } else if (bookResult.quantity === 0) {
-              res.status(200).send({
-                result: 'there is not enough quantity of this book in the library'
+                message: 'you had previously borrowed this book without returning it'
               });
             } else {
-              BorrowHistory.findOne({ // find if there is a record of this in the borrowHistory table
-                where: {
-                  userId,
-                  bookId
-                }
-              }).then((borrowHistoryInstance) => {
-                if (borrowHistoryInstance) { // if this history of borrowed book exists
-                  if (borrowHistoryInstance.returnStatus) { // if book was previously borrowed but has been returned
-                    // update borrow history instance setting returnStatus to false
-                    borrowHistoryInstance.update({ returnStatus: false })
-                      .then((updatedHistoryInstance) => {
-                        bookResult.update({ // reduce available book quantity by one
-                          quantity: bookResult.quantity - 1
-                        });
-                        res.status(200).send({
-                          updatedHistoryInstance,
-                          message: 'you have successfully borrowed this again'
-                        });
-                      }).catch(error => res.status(500).send({
-                        error,
-                        message: 'could not update this borrowing history'
-                      }));
-                  } else { // if user previously returned this book without returning it
-                    res.status(200).send({
-                      message: 'you had previously borrowed this book without returning it'
-                    });
-                  }
-                } else {
-                  BorrowHistory.create({ // create this borrow history
-                    userId,
-                    bookId
-                  }).then((historyRecord) => {
-                    bookResult.update({ // reduce available book quantity by one
-                      quantity: bookResult.quantity - 1
-                    });
-                    res.status(201).send({
-                      historyRecord,
-                      message: 'you have successfully borrowed this book'
-                    });
-                  }).catch(error => res.send(error));
-                }
-              }).catch(error => res.send(error.message));
+              req.instanceAvailable = true;
+              next();
             }
-          }).catch(error => res.send(error.message));
-        }
-      }).catch(error => res.send(error.message));
-    }
+          } else {
+            next();
+          }
+        }).catch((error) => { // should rarely happen
+          res.status(500).send({
+            error
+          });
+        });
+      }
+    }).catch((error) => { // should rarely happen
+      res.status(500).send({
+        error
+      });
+    });
   },
   // An API route that allow user to return a book
   // PUT : /api/users/<userId>/books
+
+  borrowBookAllowed(req, res) {
+    const bookId = req.body.bookId;
+
+    Book.findById(bookId).then((bookInstance) => {
+      Book.update({
+        quantity: bookInstance.quantity - 1
+      }, {
+        where: {
+          id: bookId
+        }
+      }).then(() => {
+        if (req.instanceAvailable) {
+          BorrowHistory.update({ returnStatus: false }, {
+            where: { userId: req.user, bookId: req.body.bookId }
+          }).then(() => {
+            res.status(201).send({
+              message: 'you have successfully borrowed this book again',
+              data: bookInstance
+            });
+          }).catch((error) => {
+            res.status(500).send({
+              message: 'could not complete this request',
+              error: error.message
+            });
+          });
+        } else {
+          BorrowHistory.create({
+            userId: req.user,
+            bookId: req.body.bookId
+          }).then(() => {
+            res.status(201).send({
+              message: 'you have successfully borrowed this book',
+              data: bookInstance
+            });
+          }).catch((error) => {
+            res.status(400).send({
+              message: 'could not complete this request',
+              error: error.message
+            });
+          });
+        }
+      }).catch((error) => {
+        res.status(500).send({
+          message: 'could not update this book instance',
+          error: error.message
+        });
+      });
+    }).catch((error) => {
+      res.status(500).send({
+        message: 'could not retrieve this book instance',
+        error: error.message
+      });
+    });
+  },
+
   returnBook(req, res) {
-    const userId = req.user.identifier;
+    const userId = req.user;
     const bookId = req.body.bookId;
 
     BorrowHistory.findOne({
@@ -179,25 +206,36 @@ export default {
         bookId
       }
     }).then((historyInstance) => {
-      if (historyInstance.returnStatus) {
-        res.send({
-          message: 'you had previously returned this book'
-        });
-      } else {
-        historyInstance.update({ returnStatus: true })
-          .then((updatedHistoryInstance) => {
-            Book.findById(bookId).then((bookInstance) => {
-              bookInstance.update({ quantity: bookInstance.quantity + 1 });
-            });
-            res.status(201).send({
-              updatedHistoryInstance,
-              message: 'book has been returned successfully'
-            });
-          }).catch((error) => {
-            res.status(500).send({
-              message: error.message
-            });
+      if (historyInstance) {
+        if (historyInstance.returnStatus) {
+          res.status(400).send({
+            message: 'you had previously returned this book'
           });
+        } else {
+          historyInstance.update({ returnStatus: true })
+            .then((updatedHistoryInstance) => {
+              Book.findById(bookId).then((bookInstance) => {
+                bookInstance
+                  .update({ quantity: bookInstance.quantity + 1 })
+                  .then(() => {
+                    res.status(201).send({
+                      message: 'book has been returned successfully',
+                      updatedHistoryInstance
+                    });
+                  }).catch(error => res.status(500).send({
+                    error: error.message
+                  }));
+              });
+            }).catch((error) => {
+              res.status(400).send({
+                message: error.message
+              });
+            });
+        }
+      } else {
+        res.status(400).send({
+          message: 'there is no instance of this borrowed book relationship'
+        });
       }
     }).catch((error) => {
       res.status(500).send({
