a0ff99dd207ae25ca7a5592d432e1dc13123366e
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-17 17:04:39 +0100

    Modify responses and add more validators

--- server/controllers/requestValidators.js
+++ server/controllers/requestValidators.js
@@ -1,3 +1,8 @@
+import models from '../models';
+
+const User = models.User;
+const Book = models.Book;
+
 export default {
   signUpRequest: (req, res, next) => {
     req.checkBody('username', 'username can only be 6 to 15 alphanumeric characters').notEmpty().len(6, 15).isAlphaNum();
@@ -5,17 +10,16 @@ export default {
     req.checkBody('email', 'invalid email address').notEmpty().isEmail();
     req.checkBody('membershipLevel',
       'membership level can only be in [regular, silver, gold, platinum]').inMembershipLevels();
-    
+
     req.getValidationResult().then((errors) => {
       if (!errors.isEmpty()) {
-        let errorArray = errors.useFirstErrorOnly().array();
+        const errorArray = errors.useFirstErrorOnly().array();
 
         res.status(400).send({
           message: 'invalid parameters supplied',
           description: errorArray
         });
       } else {
-        // res.send('valid input');
         next();
       }
     });
@@ -27,16 +31,175 @@ export default {
     
     req.getValidationResult().then((errors) => {
       if (!errors.isEmpty()) {
-        let errorArray = errors.useFirstErrorOnly().array();
+        const errorArray = errors.useFirstErrorOnly().array();
 
         res.status(400).send({
           message: 'invalid parameters supplied',
           description: errorArray
         });
       } else {
-        // res.send('valid input');
         next();
       }
     });
   },
+
+  createBookRequest: (req, res, next) => {
+    req.checkBody('title', 'title cannot be less than 5 characters').notEmpty().len(5, 50);
+    req.checkBody('author', 'author cannot be empty').notEmpty();
+    req.checkBody('quantity', 'quantity cannot only be a positive integer').notEmpty().isInt().isPositive();
+    req.checkBody('description', 'description cannot be less than 5 characters').notEmpty().len(5, 1000);
+    req.checkBody('category', 'category cannot be less than 3 characters').notEmpty().len(5, 20);
+    
+    // TODO: Use sanitization
+    req.getValidationResult().then((errors) => {
+      if (!errors.isEmpty()) {
+        const errorArray = errors.useFirstErrorOnly().array();
+
+        res.status(400).send({
+          message: 'invalid parameters supplied',
+          description: errorArray
+        });
+      } else {
+        next();
+      }
+    });
+  },
+
+  modifyBookRequest: (req, res, next) => {
+    req.checkParams('bookId', 'invalid bookId param').isInt().isPositive();
+    const reqBodyParams = Object.keys(req.body);
+    if (!reqBodyParams.some(value => // if request body does not include any of the book columns
+      ['title', 'description', 'author', 'quantity', 'category'].includes(value))) {
+      res.status(200).send({
+        message: 'no modifications requested'
+      });
+      return;
+    }
+
+    if (reqBodyParams.includes('title')) {
+      req.checkBody('title', 'title cannot be less than 5 characters').notEmpty().len(5, 50);
+    }
+    if (reqBodyParams.includes('author')) {
+      req.checkBody('author', 'author cannot be empty').notEmpty();
+    }
+    if (reqBodyParams.includes('quantity')) {
+      req.checkBody('quantity', 'quantity cannot only be a positive integer').notEmpty().isInt().isPositive();
+    }
+    if (reqBodyParams.includes('description')) {
+      req.checkBody('description', 'description cannot be less than 5 characters').notEmpty().len(5, 1000);
+    }
+    if (reqBodyParams.includes('category')) {
+      req.checkBody('category', 'category cannot be less than 3 characters').notEmpty().len(5, 20);
+    }
+
+    req.getValidationResult().then((errors) => {
+      if (!errors.isEmpty()) {
+        const errorArray = errors.useFirstErrorOnly().array();
+        res.status(400).send({
+          message: 'invalid parameters supplied',
+          description: errorArray
+        });
+      } else {
+        next();
+      }
+    });
+  },
+
+  adminValidation: (req, res, next) => {
+    const userId = req.user;
+
+    User.findById(userId).then((user) => {
+      if (user) { // if a user with this id exists
+        if (!user.admin) {
+          res.status(403).send({
+            error: 'you are not allowed to perform this operation'
+          });
+        } else {
+          next();
+        }
+      } else {
+        res.status(400).send({ // should rarely happen
+          error: 'this user does not exist'
+        });
+      }
+    }).catch((error) => {
+      res.status(500).send({ // should rarely happen - if there was an error retrieving the userId from the database
+        message: 'could not retrieve this user',
+        error
+      });
+    });
+  },
+
+  userBookActions: (req, res, next) => {
+    if (req.user !== req.params.userId) {
+      res.status(400).send({
+        message: 'signed in user is not equal to requesting user'
+      });
+    } else {
+      User.findById(req.params.userId)
+        .then((user) => {
+          if (!user) {
+            res.status(400).send({
+              message: 'user does not exist'
+            });
+          } else if (user.admin) {
+            res.status(400).send({
+              message: 'admin user cannot perform this function'
+            });
+          } else {
+            Book.findById(req.body.bookId)
+              .then((book) => {
+                if (!book) {
+                  res.status(400).send({
+                    message: 'there is no book with the specified id'
+                  });
+                } else {
+                  next();
+                }
+              }).catch((error) => {
+                res.status(500).send({
+                  error: error.message
+                });
+              });
+          }
+        })
+        .catch((error) => {
+          res.status(500).send({
+            error: error.message
+          });
+        });
+    }
+  },
+
+  getUserBorrowedBooks: (req, res, next) => {
+    User.findById(req.user).then((user) => {
+      if (user.admin) {
+        User.findById(req.params.userId)
+          .then((result) => {
+            if (!result) {
+              res.status(400).send({
+                message: 'user does not exist'
+              });
+            } else {
+              next();
+            }
+          })
+          .catch((error) => {
+            res.status(500).send({
+              error: error.message
+            });
+          });
+      } else if (user.id !== req.params.userId) {
+        res.status(400).send({
+          message: 'signed in user is not equal to requesting user'
+        });
+      } else {
+        next();
+      }
+    }).catch((error) => {
+      res.status(400).send({
+        error: error.errors[0].message
+      });
+    });
+  }
 };
