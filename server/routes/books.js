92d17d2b5a96772fb1892dcc9cb5aa240f5abb43
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-08-17 16:59:27 +0100

    Break controller chain for route

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,14 +1,19 @@
 import express from 'express';
 
 import bookController from '../controllers/bookController';
+import requestValidators from '../controllers/requestValidators';
 
 const router = express.Router();
 
-// POST - add book - An API route that allow users add new book
-router.post('/', bookController.addBook);
+// POST - add book - An API route that allow an admin user add a new book
+router.post('/', requestValidators.adminValidation,
+  requestValidators.createBookRequest,
+  bookController.addBook);
 
 // PUT - modify book - An API route that allow users to modify a book information
-router.put('/:bookId', bookController.modifyBook);
+router.put('/:bookId', requestValidators.adminValidation,
+  requestValidators.modifyBookRequest,
+  bookController.modifyBook);
 
 // GET - An API route that allow users to gets all the books in the library
 router.get('/', bookController.getAllBooks);
